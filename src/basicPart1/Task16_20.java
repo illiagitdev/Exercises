67d4db2ab80f80006af083ccb4cfcc4c8e10d570
illia <illya.posv.b@gmail.com>
2019-05-28 17:28:48 +0300

    +

--- src/basicPart1/Task16_20.java
+++ src/basicPart1/Task16_20.java
@@ -6,8 +6,108 @@ public class Task16_20 {
     public static void main(String[] args) {
 //        task16();
 //        task17();
-        task17a();
-        task18();
+//        task17a();
+        //todo: do task 18
+//        task18();
+//        task19();
+        task20();
+    }
+
+    private static void task20() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter value: x = ");
+        int x = scan.nextInt();
+        int z = x;
+        int i = 0;
+        int[] res = new int[20];
+
+        while (z != 0) {
+            res[i++] = z % 16;
+            z /= 16;
+        }
+
+        System.out.println("x = " + x + " result HEX:");
+
+        char[] resChar = new char[i];
+
+        i--;
+        for (int j = 0; i >= 0; i--, j++) {
+            if (res[i] < 10) {
+                resChar[j] = (char) (res[i] + 48);
+            } else {
+                switch (res[i]) {
+                    case 10: {
+                        resChar[j] = 'A';
+                        break;
+                    }
+                    case 11: {
+                        resChar[j] = 'B';
+                        break;
+                    }
+                    case 12: {
+                        resChar[j] = 'C';
+                        break;
+                    }
+                    case 13: {
+                        resChar[j] = 'D';
+                        break;
+                    }
+                    case 14: {
+                        resChar[j] = 'E';
+                        break;
+                    }
+                    case 15: {
+                        resChar[j] = 'F';
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (char c : resChar
+        ) {
+            System.out.print(c);
+        }
+        // or easy solution
+        /*int dec_num, rem;
+        String hexdec_num="";
+
+        *//* hexadecimal number digits *//*
+
+        char hex[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input a decimal number: ");
+        dec_num = in.nextInt();
+
+        while(dec_num>0)
+        {
+            rem = dec_num%16;
+            hexdec_num = hex[rem] + hexdec_num;
+            dec_num = dec_num/16;
+        }
+        System.out.print("Hexadecimal number is : "+hexdec_num+"\n");*/
+    }
+
+    private static void task19() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter value: x = ");
+        int x = scan.nextInt();
+        int z = x;
+        int i = 0;
+        int[] res = new int[20];
+
+        while (z != 0) {
+            res[i++] = z % 2;
+            z /= 2;
+        }
+
+        System.out.println("result:");
+
+        for (i--; i >= 0; i--) {
+            System.out.print(res[i]);
+        }
     }
 
     private static void task17a() {
@@ -16,13 +116,50 @@ public class Task16_20 {
         long x = scan.nextLong();
         System.out.println("Enter second value: y = ");
         long y = scan.nextLong();
-        int res = 0, i = 0;
+        int i = 0, rem = 0;
         int[] collector = new int[20];
+
+        while (x != 0 || y != 0) {
+            collector[i++] = (int) ((x % 10 + y % 10 + rem) % 2);
+            rem = (int) ((x % 10 + y % 10 + rem) / 2);
+            x /= 10;
+            y /= 10;
+        }
+        if (rem != 0) {
+            collector[i++] = rem;
+        }
+        i--;
+
+        while (i >= 0) {
+            System.out.print(collector[i--]);
+        }
     }
 
     // binary multiplication
     private static void task18() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first value: x = ");
+        long x = scan.nextLong();
+        System.out.println("Enter second value: y = ");
+        long y = scan.nextLong();
+        int i = 0, rem = 0;
+        int[] collector = new int[20];
+        long z = x;
 
+        for (int j = 0; j < 8; j++) {
+            rem = (int) (y % 10);
+            if (rem == 0) {
+                j++;
+                continue;
+            }
+            z *= (j == 0 ? 1 : 10);
+            collector = addBinary(x, z);
+        }
+
+        for (int e : collector
+        ) {
+            System.out.print(e);
+        }
     }
 
     //binary addition
@@ -47,11 +184,11 @@ public class Task16_20 {
                     collector[i] = rem;
                     rem = 0;
                 }
-            }else {
-                if(rem == 0){
+            } else {
+                if (rem == 0) {
                     collector[i] = 1;
                     rem = 0;
-                }else if (rem == 1){
+                } else if (rem == 1) {
                     collector[i] = 0;
                     rem = 1;
                 }
@@ -59,7 +196,7 @@ public class Task16_20 {
             x /= 10;
             y /= 10;
             i++;
-            if(i == 20)
+            if (i == 20)
                 break;
         }
 
@@ -70,9 +207,9 @@ public class Task16_20 {
         System.out.println("\nSum of two binary numbers = " + (res));
 
         // show number = works
-        for (int j = 19; j>= 0; j--){
-            if(collector[j] != 0){
-                for ( ; j >= 0; j--){
+        for (int j = 19; j >= 0; j--) {
+            if (collector[j] != 0) {
+                for (; j >= 0; j--) {
                     System.out.print(collector[j]);
                 }
             }
@@ -86,5 +223,21 @@ public class Task16_20 {
         System.out.println(" |  ^  | ");
         System.out.println(" | '-' | ");
         System.out.println(" +-----+ ");
+    }
+
+    private static int[] addBinary(long binary1, long binary2) {
+        int i = 0, rem = 0;
+        int[] collector = new int[20];
+
+        while (binary1 != 0 || binary2 != 0) {
+            collector[i++] = (int) ((binary1 % 10 + binary2 % 10 + rem) % 2);
+            rem = (int) ((binary1 % 10 + binary2 % 10 + rem) / 2);
+            binary1 /= 10;
+            binary2 /= 10;
+        }
+        if (rem != 0) {
+            collector[i++] = rem;
+        }
+        return collector;
     }
 }
---
+++ src/basicPart1/Task21_25.java
@@ -0,0 +1,28 @@
+package basicPart1;
+
+import java.util.Scanner;
+
+public class Task21_25 {
+    public static void main(String[] args) {
+        task21();
+    }
+
+    //Write a Java program to convert a decimal number to octal number
+    private static void task21() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter decimal number: ");
+        int number = scan.nextInt();
+        int[] octa = new int[15];
+        int rem = 0, i = 0;
+
+        while(number>0){
+            octa[i++]=number%8;
+            number/=8;
+        }
+        i--;
+        System.out.print("Octal number: ");
+        for (;i>=0;i--){
+            System.out.print(octa[i]);
+        }
+    }
+}
